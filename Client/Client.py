dacccf6b1e6e0b28378a2191e165d9c08bee1f1f
Sabari K <education@mindmy.tech>
2023-06-17 09:34:03 +0000

    Client change

--- Client/Client.py
+++ Client/Client.py
@@ -5,7 +5,7 @@ import json
 class Connect_Init:
     def __init__(self):
         self.sock = None
-        self.host_ip = "localhost"
+        self.host_ip = "70.70.70.2"
         self.port = 7080
         self.connect()
 
@@ -24,7 +24,7 @@ class Connect_Init:
         json_file = json.dumps(msg).encode()
         self.sock.send(json_file)
 
-    def Frame_POST(self, USER_ID, USERNAME, CHATROOM, MESSAGE):
+    def Frame_POST(self, USER_ID, USERNAME, MESSAGE):
         data = {'PROTOCOL': "POST",
                 'USER_ID': int(USER_ID),
                 'USERNAME': USERNAME,
@@ -32,7 +32,47 @@ class Connect_Init:
         print(data)
         self.send_msg(data)
 
-    def Frame_GET(self,  USER_ID, USERNAME, CHATROOM, L_RECORD=0):
+    def Frame_GET(self,  USER_ID, USERNAME, L_RECORD=0):
+        data = {'PROTOCOL': "POST",
+                'USER_ID': int(USER_ID),
+                'USERNAME': USERNAME,
+                "L_RECORD": L_RECORD}
+        self.send_msg(data)
+import socket
+import json
+
+
+class Connect_Init:
+    def __init__(self):
+        self.sock = None
+        self.host_ip = "70.70.70.2"
+        self.port = 7080
+        self.connect()
+
+    def connect(self):
+        print("connecting to server...")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host_ip, self.port))
+        print("connected to server")
+
+    def recv_msg(self):
+        data = self.sock.recv(1024)
+        if data:
+            return data
+
+    def send_msg(self, msg):
+        json_file = json.dumps(msg).encode()
+        self.sock.send(json_file)
+
+    def Frame_POST(self, USER_ID, USERNAME, MESSAGE):
+        data = {'PROTOCOL': "POST",
+                'USER_ID': int(USER_ID),
+                'USERNAME': USERNAME,
+                "MESSAGE": MESSAGE}
+        print(data)
+        self.send_msg(data)
+
+    def Frame_GET(self,  USER_ID, USERNAME, L_RECORD=0):
         data = {'PROTOCOL': "POST",
                 'USER_ID': int(USER_ID),
                 'USERNAME': USERNAME,
